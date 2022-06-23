6ea814aa07d1bd4e447bb9efd55bbf38bc40b2cd
mr.kvinter <mr.kvinter@gmail.com>
2022-06-23 21:04:04 +0300

    Missed files

---
+++ src/drawers/SharpCompose.Drawer.Avalonia/CashedVectorImage.cs
@@ -0,0 +1,17 @@
+﻿using SharpCompose.Drawer.Core.Images;
+using VectSharp;
+using VectSharp.SVG;
+
+namespace SharpCompose.Drawer.Avalonia;
+
+public sealed class CashedVectorImage : VectorImage
+{
+    public Graphics CashedGraphics { get; }
+
+    public CashedVectorImage(string svgSource) : base(svgSource)
+    {
+        var parsed = Parser.FromString(svgSource);
+        CashedGraphics.DrawGraphics(0, 0, parsed.Graphics);
+        CashedGraphics = parsed.Graphics;
+    }
+}
---
+++ src/drawers/SharpCompose.Drawer.WinUI/WinUIVectorImage.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Svg;
+using SharpCompose.Drawer.Core.Images;
+
+namespace SharpCompose.WinUI;
+
+public sealed class WinUIVectorImage : IImage
+{
+    public int Width { get; }
+
+    public int Height { get; }
+
+    public readonly CanvasSvgDocument CanvasSvgDocument;
+
+    public WinUIVectorImage(ICanvasResourceCreator resourceCreator, string svg)
+    {
+        CanvasSvgDocument = CanvasSvgDocument.LoadFromXml(resourceCreator, svg);
+        Width = (int)CanvasSvgDocument.Root.GetFloatAttribute("width");
+        Height = (int)CanvasSvgDocument.Root.GetFloatAttribute("height");
+    }
+}
