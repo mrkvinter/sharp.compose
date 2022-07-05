1f98e0f9fe76fc13c3d3704f36c431219ea106d2
mr.kvinter <mr.kvinter@gmail.com>
2022-07-05 20:12:42 +0300

    BoxWithConstraints was added

--- src/SharpCompose.Base/ComposesApi/LayoutCompose.cs
+++ src/SharpCompose.Base/ComposesApi/LayoutCompose.cs
@@ -1,4 +1,5 @@
-﻿using SharpCompose.Base.Layouting;
+﻿using System.Diagnostics.CodeAnalysis;
+using SharpCompose.Base.Layouting;
 using SharpCompose.Base.Modifiers;
 
 namespace SharpCompose.Base.ComposesApi;
@@ -12,22 +13,50 @@ public static partial class BaseCompose
         Composer.Instance.StopScope();
     }
 
-    public static void Box(ScopeModifier? boxModifier = default, IAlignment? alignment = null, Action? content = null) =>
+    public static void Box(ScopeModifier? boxModifier = default, IAlignment? alignment = null,
+        Action? content = null) =>
         Layout((boxModifier?.SelfModifier ?? IModifier.Empty).Then(new DebugModifier {ScopeName = nameof(Box)}),
             content,
-            BoxLayout.Measure(alignment ?? new BiasAlignment(-1, -1)));
+            BoxLayout.Measure(alignment ?? Alignment.TopStart));
 
-    public static void Column(ScopeModifier? columnModifier = default, IAlignment? horizontalAlignment = null, Action? content = null) =>
+    [SuppressMessage("ReSharper", "AccessToModifiedClosure")]
+    public static void BoxWithConstraints(ScopeModifier? boxModifier = default, IAlignment? alignment = null,
+        Action<Constraints>? content = null)
+    {
+        LayoutNode layoutNode = default!;
+        Composer.Instance.StartScope(
+            (boxModifier?.SelfModifier ?? IModifier.Empty).Then(new DebugModifier
+                {ScopeName = nameof(BoxWithConstraints)}), (_, constraints) =>
+            {
+                Composer.Instance.Scopes.Push(layoutNode);
+                content?.Invoke(constraints);
+                Composer.Instance.StopScope();
+
+                var measures = layoutNode.Children.SelectMany(e => e.Nodes.Select(e => e.Measurable)).ToArray();
+                return BoxLayout.Measure(alignment ?? Alignment.TopStart)(measures, constraints);
+            });
+
+        layoutNode = (LayoutNode)Composer.Instance.Scopes.Pop();
+    }
+
+    public static void Column(ScopeModifier? columnModifier = default, IAlignment? horizontalAlignment = null,
+        Action? content = null) =>
         Layout((columnModifier?.SelfModifier ?? IModifier.Empty).Then(new DebugModifier {ScopeName = nameof(Column)}),
-            content, 
-            RowColumnLayout.MeasureColumn(horizontalAlignment ?? new BiasAlignment(-1, 0)));
+            content,
+            RowColumnLayout.MeasureColumn(horizontalAlignment ?? Alignment.TopStart));
 
-    public static void Row(ScopeModifier? rowModifier = default, IAlignment? verticalAlignment = null, Action? content = null) =>
+    public static void Row(ScopeModifier? rowModifier = default, IAlignment? verticalAlignment = null,
+        Action? content = null) =>
         Layout((rowModifier?.SelfModifier ?? IModifier.Empty).Then(new DebugModifier {ScopeName = nameof(Row)}),
             content,
-            RowColumnLayout.MeasureRow(verticalAlignment ?? new BiasAlignment(0, -1)));
+            RowColumnLayout.MeasureRow(verticalAlignment ?? Alignment.TopStart));
 
     public static void Spacer(ScopeModifier? spacerModifier = default) =>
-        Layout((spacerModifier?.SelfModifier ?? IModifier.Empty).Then(new DebugModifier {ScopeName = nameof(Spacer)}), null,
-            BoxLayout.Measure(new BiasAlignment(-1, -1)));
+        Layout((spacerModifier?.SelfModifier ?? IModifier.Empty).Then(new DebugModifier {ScopeName = nameof(Spacer)}),
+            null, EmptyMeasure);
+
+    private static readonly Measure EmptyMeasure = (_, constraints) => new MeasureResult
+        {Width = constraints.MinWidth, Height = constraints.MinHeight, Placeable = EmptyPlaceable};
+
+    private static readonly Action<int, int> EmptyPlaceable = (_, _) => { };
 }
---
+++ tests/SharpCompose.Tests/BoxWithConstraintsTests.cs
@@ -0,0 +1,132 @@
+﻿using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SharpCompose.Base;
+using SharpCompose.Base.ComposesApi.Providers;
+using SharpCompose.Base.Extensions;
+using SharpCompose.Base.Modifiers;
+using SharpCompose.Base.Modifiers.Extensions;
+using SharpCompose.Drawer.Core.Brushes;
+using SharpCompose.Drawer.Core.Utilities;
+using TestSharpCompose.ComposeTester;
+
+namespace TestSharpCompose;
+
+public class BoxWithConstraintsTests
+{
+    [Test]
+    public async Task BoxWithConstraints_ChangeSize_ShowinTextForSmallContent()
+    {
+        var composeTester = new ComposeTester.ComposeTester(AdaptiveContent);
+
+        await composeTester.RecomposeAsync();
+        composeTester.Size = new IntSize(300, 100);
+        await composeTester.RecomposeAsync();
+        var smallContentNode = composeTester.Root.OnNodeWithText("Small content");
+        var largeContentNode = composeTester.Root.OnNodeWithText("Large content");
+
+        Assert.That(smallContentNode, Is.Not.Null);
+        Assert.That(largeContentNode, Is.Null);
+    }
+
+    [Test]
+    public async Task BoxWithConstraints_LocalProviders_CorrectLocalProviders()
+    {
+        var composeTester = new ComposeTester.ComposeTester(AdaptiveContentWithUsingProvider);
+
+        await composeTester.RecomposeAsync();
+        composeTester.Size = new IntSize(300, 100);
+        await composeTester.RecomposeAsync();
+        var smallContentNode = composeTester.Root.OnNodeWithText("Small content test-text");
+        var largeContentNode = composeTester.Root.OnNodeWithText("Large content test-text");
+
+        Assert.That(smallContentNode, Is.Not.Null);
+        Assert.That(largeContentNode, Is.Null);
+    }
+
+    [Test]
+    public async Task BoxWithConstraints_LocalProvidersWithThemes_CorrectLocalProviders()
+    {
+        var composeTester = new ComposeTester.ComposeTester(AdaptiveContentWithUsingProviderAndDarkTheme);
+
+        await composeTester.RecomposeAsync();
+        composeTester.Size = new IntSize(300, 100);
+        await composeTester.RecomposeAsync();
+        var smallContentNode = composeTester.Root.OnNodeWithText("Small content test-text")!;
+        var largeContentNode = composeTester.Root.OnNodeWithText("Large content test-text");
+        var textDrawModifier = (TextDrawModifier) smallContentNode.LayoutNode.Modifier
+            .SqueezeModifiers().First(e => e is TextDrawModifier);
+        var smallContentColor = ((SolidColorBrush) textDrawModifier.Brush).Color;
+
+        Assert.That(smallContentColor, Is.EqualTo(Color.Black));
+        Assert.That(largeContentNode, Is.Null);
+    }
+
+    [Test]
+    public async Task BoxWithConstraints_LocalProvidersWithChangedTheme_CorrectLocalProviders()
+    {
+        var composeTester = new ComposeTester.ComposeTester(AdaptiveContentWithUsingProviderAndDarkTheme);
+
+        await composeTester.RecomposeAsync();
+        composeTester.Size = new IntSize(300, 100);
+        composeTester.Root.OnNodeWithId("ChangeThemeButton")!.PerformClick();
+        await composeTester.RecomposeAsync();
+        var smallContentNode = composeTester.Root.OnNodeWithText("Small content test-text")!;
+        var largeContentNode = composeTester.Root.OnNodeWithText("Large content test-text");
+        var textDrawModifier = (TextDrawModifier) smallContentNode.LayoutNode.Modifier
+            .SqueezeModifiers().First(e => e is TextDrawModifier);
+        var textColor = ((SolidColorBrush) textDrawModifier.Brush).Color;
+
+        Assert.That(textColor, Is.EqualTo(Color.White));
+        Assert.That(largeContentNode, Is.Null);
+    }
+
+    [Composable]
+    private static void AdaptiveContent() => BoxWithConstraints(content: constraints =>
+    {
+        if (constraints.MaxWidth < 500)
+            Box(content: () => Text("Small content"));
+        else
+            Box(content: () => Text("Large content"));
+    });
+
+    private static readonly LocalProvider<string> LocalTestTextProvider = new(string.Empty);
+
+    [Composable]
+    private static void AdaptiveContentWithUsingProvider() => CompositionLocalProvider(new[]
+    {
+        LocalTestTextProvider.Provide("test-text"),
+    }, content: () => BoxWithConstraints(content: constraints =>
+    {
+        if (constraints.MaxWidth < 500)
+            Box(content: () => Text($"Small content {LocalTestTextProvider.Value}"));
+        else
+            Box(content: () => Text($"Large content {LocalTestTextProvider.Value}"));
+    }));
+
+
+    [Composable]
+    private static void AdaptiveContentWithUsingProviderAndDarkTheme() => Column(content: () =>
+    {
+        var isLight = Remember.Get(() => true.AsMutableState());
+        var darkTheme = new Colors(Color.Navy, Color.White, Color.Black, Color.White, Color.Black);
+        var lightTheme = new Colors(Color.Aqua, Color.Black, Color.White, Color.Black, Color.White);
+        var colors = isLight.Value ? lightTheme : darkTheme;
+        CompositionLocalProvider(new[]
+        {
+            LocalTestTextProvider.Provide("test-text"),
+            LocalColors.Provide(colors)
+        }, content: () => Box(Modifier.BackgroundColor(LocalColors.Value.Background), content: () =>
+        {
+            Button(() => isLight.Value = !isLight.Value, "Change Theme", Modifier.Id("ChangeThemeButton"));
+            BoxWithConstraints(content: constraints =>
+            {
+                if (constraints.MaxWidth < 500)
+                    Box(content: () => Text($"Small content {LocalTestTextProvider.Value}"));
+                else
+                    Box(content: () => Text($"Large content {LocalTestTextProvider.Value}"));
+            });
+        }));
+    });
+}
